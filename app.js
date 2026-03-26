e65584fd3321356f727b80cca1aa0da0b43b347c
GabNader <gabriel.marini21@gmail.com>
2026-03-25 21:41:51 -0300

    25-03

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ const flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var userRoutes = require("./modules/user/userRoutes");
 
 var app = express();
+var expressLayouts = require("express-ejs-layouts"); 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views/pages"));
+app.set('layout', path.join(__dirname, 'views/layouts/main'));
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,13 +32,15 @@ app.use(session({
 app.use(flash());
 app.use((req, res, next) => { 
   res.locals.messages = req.flash();
+  res.locals.user = req.session.user || null;
   next();
 });
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", userRoutes);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- modules/user/userController.js
+++ modules/user/userController.js
@@ -1,5 +1,7 @@
 const User = require('./userModel');
 const bcrypt = require('bcryptjs');
+const fs = require("fs");
+const path = require("path");
 
 exports.register = async (req, res) => {
     const { username, email, password, confirmPassword, fullName } = req.body;
@@ -104,7 +106,18 @@ exports.updateProfile = async (req, res) => {
             updateData.profilePicture = req.file.filename;
         }
 
+        const oldUser = await User.findByPk(userId);
+
         await User.update(updateData, { where: { id: userId } });
+
+        if (req.file && oldUser.profilePicture && oldUser.profilePicture !== 'default-profile.png') {
+        const oldProfilePicPath = path.join(__dirname, '../../public/uploads/profiles', oldUser.profilePicture);
+        fs.unlink(oldProfilePicPath, (err) => {
+        if (err) console.error('Erro ao apagar foto de perfil antiga:', err);
+        else console.log('Foto de perfil antiga apagada:', oldProfilePicPath);
+        });
+        }
+
 
         req.flash('success', 'Perfil atualizado com sucesso!');
         res.redirect('/profile/edit');
